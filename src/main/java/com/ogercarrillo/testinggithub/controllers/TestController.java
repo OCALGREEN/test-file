9be51f9ff77e2ee9a55fa7d2bbab534e8dbd281b
Oger Carrillo <ogercarrillo@gmail.com>
2023-02-02 14:47:19 -0800

    testing-java-github

---
+++ src/main/java/com/ogercarrillo/testinggithub/controllers/TestController.java
@@ -0,0 +1,19 @@
+package com.ogercarrillo.testinggithub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class TestController {
+	
+	@GetMapping("/")
+	public String test() {
+		return "test.jsp"; 
+	}
+	
+	@GetMapping("/test")
+	public String test1() {
+		return "test.jsp"; 
+	}
+
+}
