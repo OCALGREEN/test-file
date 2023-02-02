2104c0f660e9a2bdeefb63828958e178c12805ca
Oger Carrillo <ogercarrillo@gmail.com>
2023-02-02 14:49:16 -0800

    commiting all

---
+++ src/main/java/com/ogercarrillo/testinggithub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ogercarrillo.testinggithub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestFileApplication.class);
+	}
+
+}
---
+++ src/test/java/com/ogercarrillo/testinggithub/TestFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogercarrillo.testinggithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
