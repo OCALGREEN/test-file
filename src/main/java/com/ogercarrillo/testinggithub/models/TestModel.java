013e9f8b009e532791a92ebff2844e5eebea5c79
Oger Carrillo <ogercarrillo@gmail.com>
2023-02-02 15:02:05 -0800

    adding models

---
+++ src/main/java/com/ogercarrillo/testinggithub/models/TestModel.java
@@ -0,0 +1,17 @@
+package com.ogercarrillo.testinggithub.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="test") 
+public class TestModel {
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id; 
+
+}
